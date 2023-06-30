8ed30c0e3eb55af3b06e0e6c19ecae3ccc6bf31a
Xavier Dharmaiyan <xavier@redhat.com>
2023-06-30 08:34:29 -0400

    added batched cluster query
    Signed-off-by: Xavier Dharmaiyan <xavier@redhat.com>
    Signed-off-by: Darshan Vandra <dvandra@redhat.com>

--- queries.py
+++ queries.py
@@ -6,37 +6,34 @@ GB_FACTOR = 1e9
 
 resolution=os.environ.get('RESOLUTION', '10m')
 # Queries
+
+CLUSTER_IDS = "cluster_version" 
+
 RESOURCE_REQUEST = f"""
     sum by ({ID_LABEL}, namespace, resource)
-    (kube_pod_container_resource_requests:sum)
+    (kube_pod_container_resource_requests:sum{{{ID_LABEL}=~'CLUSTER_FILTER', namespace!=''}})
 """
 CPU_USAGE_MAX = f"""
     max_over_time(
       sum by ({ID_LABEL}, namespace)
-      (node_namespace_pod_container:container_cpu_usage_seconds_total:sum)[1d:{resolution}]
+      (node_namespace_pod_container:container_cpu_usage_seconds_total:sum{{{ID_LABEL}=~'CLUSTER_FILTER', namespace!=''}})[1d:{resolution}]
+    )
+"""
+CPU_USAGE_AVG = f"""
+    avg_over_time(
+      sum by ({ID_LABEL}, namespace)
+      (node_namespace_pod_container:container_cpu_usage_seconds_total:sum{{{ID_LABEL}=~'CLUSTER_FILTER', namespace!=''}})[1d:{resolution}]
     )
 """
 MEMORY_USAGE_MAX = f"""
     max_over_time(
       sum by ({ID_LABEL}, namespace)
-      (container_memory_working_set_bytes)[1d:{resolution}]
-    )
-"""
-# MEMORY_USAGE_MAX = f"""
-#     max_over_time(
-#       sum by ({ID_LABEL}, namespace)
-#       (namespace:container_memory_usage_bytes:sum)[1d:{resolution}]
-#     )
-# """
-CPU_USAGE_AVG = f"""
-    avg_over_time(
-      sum by ({ID_LABEL}, namespace)
-      (node_namespace_pod_container:container_cpu_usage_seconds_total:sum)[1d:{resolution}]
+      (container_memory_working_set_bytes{{{ID_LABEL}=~'CLUSTER_FILTER', namespace!=''}})[1d:{resolution}]
     )
 """
 MEMORY_USAGE_AVG = f"""
     avg_over_time(
       sum by ({ID_LABEL}, namespace)
-      (container_memory_working_set_bytes)[1d:{resolution}]
+      (container_memory_working_set_bytes{{{ID_LABEL}=~'CLUSTER_FILTER', namespace!=''}})[1d:{resolution}]
     )
 """
