7163083cca0155d42239020b977ba07710e62324
Darshan Vandra <dvandra@redhat.com>
2023-09-27 19:47:06 +0530

    add default grafana_uid

--- recommend.sh
+++ recommend.sh
@@ -4,6 +4,6 @@ export METRIC_DAYS=10
 export RESOLUTION=20m
 export LOGROOT=./logs
 export BATCH_SIZE=4
-export GRAFANA_DASHBOARD_UID=""
+export GRAFANA_DASHBOARD_UID="85a562078cdf77779eaa1add43ccec1e"
 rm -rf ./logs
 python start.py 
